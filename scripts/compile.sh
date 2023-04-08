350c722da108f8a6c8055ab404349e832f109b6c
Dustin <dc.christianson@gmail.com>
2023-04-08 16:32:15 -0600

    added build and run scripts, cleaned up messy compilations

---
+++ scripts/compile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This script is used to compile the project.
+
+javac ../com/craftinginterpreters/lox/*.java
+
---
+++ scripts/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This script is used to run the project.
+
+cd /home/dustin/2023-projects/lox/
+
+java com.craftinginterpreters.lox.Lox
