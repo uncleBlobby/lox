9bdf5aebd7e4c238bab74163253b0e377aa9376f
Dustin <dc.christianson@gmail.com>
2023-04-08 18:23:59 -0600

    finished parser

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -2,5 +2,11 @@
 
 # This script is used to compile the project.
 
-javac ../com/craftinginterpreters/lox/*.java
+javac /home/dustin/2023-projects/lox/com/craftinginterpreters/lox/AstPrinter.java
+javac /home/dustin/2023-projects/lox/com/craftinginterpreters/lox/Lox.java
+javac /home/dustin/2023-projects/lox/com/craftinginterpreters/lox/Parser.java
+javac /home/dustin/2023-projects/lox/com/craftinginterpreters/lox/Scanner.java
+javac /home/dustin/2023-projects/lox/com/craftinginterpreters/lox/Token.java
+javac /home/dustin/2023-projects/lox/com/craftinginterpreters/lox/TokenType.java
+
 
